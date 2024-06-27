96766501390cb19cebd3c4c0d1c32e4489c6e2be
fadlinux <n5210_fadli@yahoo.com>
2024-06-27 14:24:02 +0700

    chore: add database sql

--- database.sql
+++ database.sql
@@ -1,12 +1,29 @@
--- This is the SQL script that will be used to initialize the database schema.
--- We will evaluate you based on how well you design your database.
--- 1. How you design the tables.
--- 2. How you choose the data types and keys.
--- 3. How you name the fields.
--- In this assignment we will use PostgreSQL as the database.
+CREATE TABLE estate (
+    id uuid,
+    width integer,
+    length integer,
+    create_time timestamptz,
+    update_time timestamptz
+);
 
--- This is test table. Remove this table and replace with your own tables. 
-CREATE TABLE test (
-	id serial PRIMARY KEY,
-	name VARCHAR ( 50 ) UNIQUE NOT NULL,
+create table tree
+(
+    id         uuid,
+    width      int,
+    length     int,
+    height     int,
+    estate_id  uuid,
+    create_time timestamptz,
+    update_time timestamptz
+);
+
+create table stats
+(
+    tree_id    uuid,
+    width      int,
+    length     int,
+    height     int,
+    estate_id  uuid,
+    create_time timestamptz,
+    update_time timestamptz
 );
